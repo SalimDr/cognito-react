9a5e586319bccdaa0024d00d983677f8108e4fb8
Salim Draoui <draouiselim@gmail.com>
2023-11-03 09:46:11 +0100

    fix app test npm

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/AWS Amplify/i);
   expect(linkElement).toBeInTheDocument();
 });
